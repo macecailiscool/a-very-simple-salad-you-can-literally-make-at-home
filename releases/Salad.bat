fb0424c7d95b6a1d74f6b054c5498d8988e4b628
Mace Cail <116770955+macecailiscool@users.noreply.github.com>
2023-05-31 14:37:33 -0500

    Create Salad.bat

---
+++ releases/Salad.bat
@@ -0,0 +1,18 @@
+@echo off
+echo A Very Simple Salad That You Can Literally Make At Home
+echo.
+echo Ingredients:
+echo - Lettuce: Choose your preferred type of lettuce, such as romaine, iceberg, or mixed greens, as the base of your salad.
+echo - Ranch dressing: Use a small amount of ranch dressing to add creaminess and tangy flavor to the salad. Adjust the quantity according to your taste preferences.
+echo - Cheese: A combination of Swiss, mozzarella, and sharp cheddar cheese. This blend of cheeses provides a variety of flavors and textures, with Swiss offering a mild nuttiness, mozzarella adding a smooth and elastic texture, and sharp cheddar providing a bold and tangy taste.
+echo - Tomatoes: Include fresh tomatoes for a burst of juiciness and added flavor. You can slice them or dice them based on your preference.
+echo.
+echo Instructions:
+echo 1. Wash and dry the lettuce leaves. Tear or chop them into bite-sized pieces and place them in a salad bowl.
+echo 2. Slice or dice the tomatoes and add them to the bowl with the lettuce.
+echo 3. Sprinkle the cheese blend over the lettuce and tomatoes.
+echo 4. Drizzle a small amount of ranch dressing over the salad ingredients.
+echo 5. Use salad tongs or a large spoon to gently toss and mix all the ingredients together, ensuring the dressing and cheese are evenly distributed.
+echo 6. Once everything is well combined, you can serve the salad immediately.
+echo.
+echo Remember, you can always customize this recipe by adding other vegetables like cucumbers, carrots, or bell peppers, or including protein sources like grilled chicken or chickpeas for a more substantial meal. Adjust the quantities of each ingredient to suit your taste and preferences. Enjoy your delicious homemade salad!
